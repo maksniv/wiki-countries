4be3512548de76b625f5f8b7f9a207b3df149dc0
Maksim <maksniv@gmail.com>
2023-06-02 19:10:08 +0300

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import AboutView from '../views/AboutView.vue';
 
 const routes = [
   {
-    path: '/wiki-countries/',
+    path: '/wiki-countries/wiki-countries/',
     name: 'home',
     component: HomeView,
   },
